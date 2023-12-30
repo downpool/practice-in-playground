1df82a667d6d368335516ef993f81cb1fc784927
donghyeon choi <downpool.choi@gmail.com>
2023-12-30 17:35:48 +0900

    init

--- practice.playground/Contents.swift
+++ practice.playground/Contents.swift
@@ -1,53 +1,35 @@
 import Foundation
+import SwiftUI
 
-struct Human: Codable {
-    var name: String
-    var age: Int
+
+//async -> 비동기 함수로 작동한다고 알려주고, 중단될 수 있다.
+//await -> 비동기 함수의 중단되는 위치 표시
+
+
+func someWork() async -> Int {
+    
+    try? await Task.sleep(nanoseconds: 1000 * 1000 * 1000 * 5)
+    
+    return 5
 }
 
-let oneMan: Human = .init(name: "downpool", age: 23)
-
-let encodes = try? JSONEncoder().encode(oneMan)
-
-let json = NSString(data: encodes!, encoding: String.Encoding.utf8.rawValue)
-//print(type(of: json!))
-
-let datas = """
-{ "name" : "downpool",
-    "age": 23}
-""".data(using: .utf8)!
-
-let decodes = try? JSONDecoder().decode(Human.self, from: datas)
-
-decodes?.name
-decodes?.age
-//
-//let array: [Int] = [1, 2, 3, 4, 2, 3, 1, 5, 4, 12, 10, 8]
-//
-////print(array.last!, terminator: "")
-//
-//var array1: [Int] = array.shuffled()
-//
-//for ints in array1 {
-//    print("\(ints)")
-//}
-var arrays: [Int] = []
-for i in 1 ... 45 {
-    arrays.append(i)
+func simpleWork() {
+    print("dddd" + "dddd" + "d d")
 }
-var appendsArray: [Int] = []
-for j in 0...4{
-    var lottery = arrays.shuffled()
-    for i in 0 ..< 6 {
-        appendsArray.append(lottery[i])
+
+func start() {
+    
+    Task {
+        print("start")
+        let value = await someWork()
+        print(value)
+        print("finish")
+        //xxx
+        //xxx
     }
-
-    appendsArray.sort()
-
-    for i in appendsArray {
-        print(i, terminator: " ")
-    }
-    print()
-    appendsArray.removeAll()
+    
+    simpleWork()
 }
 
+
+start()
